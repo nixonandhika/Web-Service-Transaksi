c9570277bfa92311c4445bd5d244071b784cfaa0
xion <nixonandhika28@gmail.com>
2019-11-24 15:49:57 +0700

    empty database

--- database/transactions.sql
+++ database/transactions.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 23, 2019 at 02:29 PM
--- Server version: 10.1.30-MariaDB
--- PHP Version: 7.2.1
+-- Host: 127.0.0.1
+-- Generation Time: Nov 24, 2019 at 09:48 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -38,19 +38,6 @@ CREATE TABLE `transactions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `transactions`
---
-
-INSERT INTO `transactions` (`id_transaksi`, `id_pengguna`, `id_schedule`, `no_kursi`, `waktu_transaksi`, `status`) VALUES
-(4, 1, 1, 2, '2019-10-29 07:19:05', 0),
-(5, 1, 5, 2, '2019-10-29 07:20:46', 0),
-(6, 1, 7, 2, '2019-10-29 07:27:29', 0),
-(7, 1, 2, 2, '2019-10-29 09:48:53', 0),
-(8, 2, 10, 25, '2019-11-08 20:08:50', 2),
-(9, 14, 1, 1, '2019-11-23 00:00:00', 0),
-(10, 2, 1, 12, '2019-11-23 13:08:32', 1);
-
---
 -- Indexes for dumped tables
 --
 
@@ -68,7 +55,7 @@ ALTER TABLE `transactions`
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id_transaksi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_transaksi` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
